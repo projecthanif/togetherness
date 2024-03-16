91cdd54fa0914d888d7cf0f23f54fb575ede7a10
projecthanif <iamustapha213@gmail.com>
2024-03-16 16:33:15 +0100

    all working remain some changes

---
+++ resources/views/services.blade.php
@@ -0,0 +1,53 @@
+@extends('app')
+
+@section('app')
+    <main>
+        <!-- ===== Blog Single Start ===== -->
+        <section class="gj qp gr hj rp hr">
+            <!-- ===== CTA Start ===== -->
+            <section class="i pg gh ji">
+                <!-- Bg Shape -->
+                <img class="h p q" src="images/shape-16.svg" alt="Bg Shape" />
+                <div class="bb ye i z-10 ki xn dr">
+                    <div class="tc uf sn tn un gg">
+                        <div class="animate_left">
+                            <h2 class="fk vj zp pr lk ac">
+                                OUR SERVICE
+                            </h2>
+                            <p class="lk">
+                                WE BELIEVE THAT EVERYONE HAS A RIGHT TO LIVE A PURPOSE FULFILLING LIFE
+                                <br> <br>
+                                WE PROVIDE EASY ACCESS TO EDUCATION, SKILL TRAINING AND SUPPORT SCHEME FOR HUMAN CAPITAL
+                                DEVELOPMENT… FOR BETTER AFRICA
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- ===== CTA End ===== -->
+            <div class="bb ze ki xn 2xl:ud-px-0">
+                <div class="tc sf yo zf kq">
+                    <div
+                        class="animate_top rounded-md shadow-solid-13 bg-white dark:bg-blacksection border border-stroke dark:border-strokedark p-7.5 md:p-10">
+                        <h2 class="ek vj 2xl:ud-text-title-lg kk wm nb gb">SUPREME DIGITAL ACADEMY</h2>
+                        <p>
+                            Supreme Digital Academy is an innovative online college that offer training on entrepreneurship,
+                            leadership and technology. It is a professional online learning centre were professionals
+                            partner to educate and inspire young people with new creative and innovative skills that are on
+                            high demand in today’s digital-space.
+                        </p>
+                        <h2 class="ek vj 2xl:ud-text-title-lg kk wm nb qb">SUPREME INNOVATION HUB
+                        </h2>
+                        <p>
+                            Supreme Innovation Hub is a digital community of technology entrepreneurs, experts and
+                            enthusiasts created by TSN_AFRICAN to bond a forum of innovation growth.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ===== Blog Single End ===== -->
+        <!-- ===== CTA End ===== -->
+    </main>
+@endsection
